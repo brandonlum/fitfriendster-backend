b943e78b071c5c5a41ececa1a2b94fd60d99639d
brandonlum <brandon.lum09@gmail.com>
2019-08-20 19:48:13 -0400

    Pushing basic front-end

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    user = {'username': 'Miguel'}
-    return '''
-<html>
-    <head>
-        <title>Home Page - FitFriend </title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username']+'''!</h1>
-    </body>
-</html>'''
+    user = {'username': 'Roger'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title="Home-Page", user='', posts=posts)
+
+
