836521c9e60dc419f87e9c51aa5fae318124aa50
brandonlum <brandon.lum09@gmail.com>
2019-08-21 00:24:51 -0400

    Added dynamic routing and validation/error message

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -17,12 +17,20 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title="Home-Page", user='', posts=posts)
+    return render_template('index.html', title="Home-Page", user='', posts='')
 
 
-@app.route('/login')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.form(
+            form.username.data, form.remember_me
+        ))
+        return redirect (url_for('index'))
+    # else:
+    #     flash('Login Failed')
     return render_template('login.html', title="Sign In", form=form)
 
 
